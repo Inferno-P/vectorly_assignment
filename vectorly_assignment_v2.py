b937e3f97a4b97c6da278e7197ac177ecb5b3b86
Adamya Tripathi <adamyatripathi@gmail.com>
2020-02-28 19:40:42 +0530

    Improved submission with documentation

--- vectorly_assignment_v2.py
+++ vectorly_assignment_v2.py
@@ -19,15 +19,16 @@ def getTextOverlay(input_image):
     # Filtering basis a RGB range
     lower = np.array([0, 0, 0])
     upper = np.array([20, 20, 20])
-    median_mask = cv2.inRange(1*median_blur, lower, upper) # TOGGLE the upper_range and lower_range 
+    median_mask = cv2.inRange(2*median_blur, lower, upper) # TOGGLE the upper_range and lower_range 
     
     # Dilating the image to compensate for the loss of some (boundary)pixels due to blurring.
     kernel = np.ones((5,5), np.uint8)
-    Median_Mask_dilation = cv2.dilate(median_mask, kernel, iterations=1)
+    Median_Mask_dilation = cv2.dilate(median_mask, kernel, iterations=1)  # Version 0 solution
     
-    # Additional ways to improve the output
-    Median_Mask_dilation_opening = cv2.morphologyEx(Median_Mask_dilation, cv2.MORPH_OPEN, kernel) 
-    Median_Mask_dilation_closing  = cv2.morphologyEx(Median_Mask_dilation, cv2.MORPH_CLOSE, kernel) 
+    # Additional ways to improve the output 
+    # Dilation and Opening are morphological operations that tend to be helpful in 'polishing' various irrelevant # artifacts like bumps on the outer boundary and depressions on the inner boundary.
+    Median_Mask_dilation_opening = cv2.morphologyEx(Median_Mask_dilation, cv2.MORPH_OPEN, kernel)  # Improvement
+    Median_Mask_dilation_closing  = cv2.morphologyEx(Median_Mask_dilation, cv2.MORPH_CLOSE, kernel) # Improvement 
     
     """
     while True :
@@ -41,8 +42,30 @@ def getTextOverlay(input_image):
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    """    
+    """ 
+    
+    
     output = 255 - Median_Mask_dilation_closing
+    
+    
+    
+    """
+    Observation : To the best of my knowledge, vectorly aims to achieve most satisfying results with minimum processing. Hence, I have tried to provide my solution using the quite basic techniques in Image Processing
+    which are not compute-intensive. 
+    
+    Next steps would be using algorithms that draw inferences based on the structures within the pixel groups rather than colorspaces. 
+    For eg: 
+    Alternate Approach:
+        1. Converting the image into a grayscale.
+        
+        2. Applying Canny Edge.
+            2.1 Applying Dilation and Opening operations to resolve broken boundary issues, if required.
+        
+        3. Performing Contour tracing. Since edges of pixel groups inside text characters form closed polygons, we can count the number of contours that form closed polygons.
+        
+        4. Transfering all the closed contours into a new image with white background and filling them with black colors.
+    
+    """
     return output
 
 if __name__ == '__main__':
